8028bea1535febacf2ef645c934f2abf1098b102
Gareth Nott <gnott@uccs.edu>
2023-04-18 17:38:33 +0000

    Devise working

---
+++ db/migrate/20230418172827_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :type
+      t.text :ingredients
+      t.text :instructions
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
